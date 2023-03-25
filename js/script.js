2005b6a37c3120ebca6e08c28a838027993856bc
Vladislav <dubovecvladislav0@gmail.com>
2023-03-25 15:34:08 +0300

    Create slider without animation on arrows

--- js/script.js
+++ js/script.js
@@ -1,6 +1,49 @@
-$(document).ready(function(){
-    $('.header__burger').click(function(event){
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
         $('.header__burger,.header__list-link').toggleClass('active');
         $('body').toggleClass('lock')
     });
+});
+
+
+
+let myCarSlider = new Swiper('.body__slider', {
+    navigation: {
+        nextEl: '.body-button-next',
+        prevEl: '.body-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+
+    mousewheel: {
+        sensitivity: 1,
+    },
+
+    speed: 600,
+    // loop: true,
+});
+
+let mySliderAllSlides = document.querySelector('.body__slider-total');
+let mySliderCurrentSlide = document.querySelector('.body__slider-current');
+
+mySliderAllSlides.innerHTML = myCarSlider.slides.length;
+
+myCarSlider.on('slideChange', function () {
+    let currentSlide = ++myCarSlider.realIndex;
+    mySliderCurrentSlide.innerHTML = currentSlide;
 });
